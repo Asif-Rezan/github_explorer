1041c2d4f7bae6b39860f8a629a039651de3c6a3
Asif-Rezan <asifrezan@gmail.com>
2025-11-05 21:19:12 +0600

    feat: complete first page

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:github_explorer/features/first_page/presentation/view/screen/first_screen.dart';
 import '../../core/constant/route_names.dart';
 import '../../features/home/presentation/view/screen/home_screen.dart';
 import '../../features/splash_screen/splash_screen.dart';
@@ -14,6 +15,10 @@ class AppRoutes {
     GetPage(
       name: RouteNames.home,
       page: () => const HomeScreen(),
+    ),
+    GetPage(
+      name: RouteNames.firstScreen,
+      page: () => FirstScreen(),
     ),
   ];
 }
---
+++ lib/features/first_page/presentation/viewmodel/first_screen_viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class FirstScreenViewModel extends GetxController {
+  var username = ''.obs;
+
+  void setUsername(String value) {
+    username.value = value.trim();
+  }
+}
